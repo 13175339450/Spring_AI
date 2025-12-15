9c7bd0930463e660f22cf8bfe6ddaa02e54243c5
hxl <hxl@git.com>
2025-12-15 23:02:51 +0800

    [2025/12/15]ChatClient多模型配置

--- SAA-04-MultiModel/src/main/java/com/hxl/spring/ai/config/MultiModelConfig.java
+++ SAA-04-MultiModel/src/main/java/com/hxl/spring/ai/config/MultiModelConfig.java
@@ -3,7 +3,9 @@ package com.hxl.spring.ai.config;
 import com.alibaba.cloud.ai.dashscope.api.DashScopeApi;
 import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatModel;
 import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatOptions;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -26,6 +28,8 @@ public class MultiModelConfig {
 
     private static final String QWEN = "qwen3-max";
 
+    // ---------------------------------- 配置 ChatModel ----------------------------------
+
     @Bean
     public ChatModel deepseekModel() {
         return DashScopeChatModel.builder()
@@ -40,5 +44,20 @@ public class MultiModelConfig {
                 .dashScopeApi(DashScopeApi.builder().apiKey(apiKey).build())
                 .defaultOptions(DashScopeChatOptions.builder().withModel(QWEN).build())
                 .build();
+    }
+
+    // ---------------------------------- 配置 ChatClient ----------------------------------
+
+    /**
+     * TODO: @Qualifier 明确指定注入名称为 deepseekModel 的那个 Bean
+     */
+    @Bean
+    public ChatClient deepseekClient(@Qualifier("deepseekModel") ChatModel deepseekModel) {
+        return ChatClient.builder(deepseekModel).build();
+    }
+
+    @Bean
+    public ChatClient qwenClient(@Qualifier("qwenModel") ChatModel qwenModel) {
+        return ChatClient.builder(qwenModel).build();
     }
 }
--- SAA-04-MultiModel/src/main/java/com/hxl/spring/ai/controller/MultiModelController.java
+++ SAA-04-MultiModel/src/main/java/com/hxl/spring/ai/controller/MultiModelController.java
@@ -1,6 +1,7 @@
 package com.hxl.spring.ai.controller;
 
 import jakarta.annotation.Resource;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,13 +25,40 @@ public class MultiModelController {
     @Resource
     private ChatModel qwenModel;
 
+    @Resource
+    private ChatClient deepseekClient;
+
+    @Resource
+    private ChatClient qwenClient;
+
+
+    // ------------------------------ ChatModel -----------------------------
+
+    @GetMapping("/qwen")
+    public Flux<String> qwen(@RequestParam("msg") String msg) {
+        return qwenModel.stream(msg);
+    }
+
     @GetMapping("/deepseek")
     public Flux<String> deepseek(@RequestParam("msg") String msg) {
         return deepseekModel.stream(msg);
     }
 
-    @GetMapping("/qwen")
-    public Flux<String> qwen(@RequestParam("msg") String msg) {
-        return qwenModel.stream(msg);
+    // ------------------------------ ChatModel -----------------------------
+
+    @GetMapping("/qwenClient")
+    public Flux<String> qwenClient(@RequestParam("msg") String msg) {
+        return qwenClient.prompt()
+                .user(msg)
+                .stream()
+                .content();
+    }
+
+    @GetMapping("/deepseekClient")
+    public Flux<String> deepseekClient(@RequestParam("msg") String msg) {
+        return deepseekClient.prompt()
+                .user(msg)
+                .stream()
+                .content();
     }
 }
